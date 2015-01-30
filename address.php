fb0eb4e262530aae47c0be381bac24c2bf5253ec
af3pj <af3pj@virginia.edu>
2015-01-30 12:03:43 -0500

    address variable added

--- address.php
+++ address.php
@@ -13,7 +13,8 @@
 
 <?php 
 
-echo "Andrew Ferguson<br>Bryan Hall 219<br>P.O. Box 400121<br>Charlottesville VA 22904"; 
+$address = "Andrew Ferguson<br>Bryan Hall 219<br>P.O. Box 400121<br>Charlottesville VA 22904";
+echo $address;
 
 ?>
 
