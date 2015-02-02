6ef501a2b039daf68b76b7102bfb70978a259ea3
af3pj <af3pj@virginia.edu>
2015-02-02 05:28:26 -0500

    URL and index commit

---
+++ url.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+
+	<title>Phobia</title>
+
+</head>
+
+<body>
+
+	<h3>What do you fear?</h3>
+<p>
+
+<?php 
+
+$fear = $_GET['fear'];
+
+echo $fear;
+
+?>
+
+</p>
+
+</body>
+
+</html>
